bcf1211c9643335a504e43a078faff7a46c94e5c
tcardoso0 <80537078+tcardoso0@users.noreply.github.com>
2026-06-04 01:51:35 +0000

    nivel novato concluido

--- chef_sort.c
+++ chef_sort.c
@@ -35,8 +35,42 @@ typedef struct {
 // Dica: Use strcmp() da biblioteca <string.h> para comparar strings.
 // Dica: Passe ponteiros para contadores de comparações e trocas se quiser alterá-los dentro da função.
 void bubbleSortStrings(char arr[][50], int n, int *comparacoes, int *trocas) {
-    // Sua lógica do Bubble Sort aqui
+    int i, j;
+    int trocou; // variavel de controle
+    char temp[50]; // variavel temporaria para troca de strings
+    
+    // contadores
+    *comparacoes = 0;
+    *trocas = 0;
+
+    // laco externo: controle de passagens
+    for (i = 0; i < n - 1; i++) {
+        trocou = 0;
+
+        // laco interno: compara elementos
+        for (j = 0; j < n - 1 - i; j++) { 
+            
+            (*comparacoes)++; 
+
+            // comparacao alfabetica das strings
+            if (strcmp(arr[j], arr[j+1]) > 0) {
+                // Realiza a troca usando strcpy
+                strcpy(temp, arr[j]);
+                strcpy(arr[j], arr[j+1]);
+                strcpy(arr[j+1], temp);
+
+                (*trocas)++; // incrementa o valor apontado por 'trocas'
+                trocou = 1;  // sinaliza que houve movimentação
+            }
+        }
+
+        // se a passagem terminou sem trocas, a lista já está ordenada (melhor caso O(n))
+        if (trocou == 0) {
+            break;  
+        }
+    }
 }
+
 
 // Nível Aventureiro: Selection Sort para array de structs (Prato)
 /*
@@ -64,7 +98,7 @@ int main() {
     // ---------------------------------------------------------
     // ÁREA DO NÍVEL NOVATO (Despensa / Bubble Sort)
     // ---------------------------------------------------------
-    /*
+    
     char ingredientes[5][50] = {"Tomate", "Cebola", "Alho", "Cenoura", "Batata"};
     int num_ingredientes = 5;
     int comparacoes = 0;
@@ -73,13 +107,25 @@ int main() {
     printf("--- Nivel Novato: Organizando a Despensa ---\n");
     printf("Lista ANTES da ordenacao:\n");
     // Laco para imprimir ingredientes antes
+    for(int i = 0; i < num_ingredientes; i++) {
+        printf("%d. %s\n", i + 1, ingredientes[i]);
+    }
 
     // Chamada da funcao bubbleSortStrings(...)
+    bubbleSortStrings(ingredientes, num_ingredientes, &comparacoes, &trocas);
 
     printf("\nLista DEPOIS da ordenacao:\n");
     // Laco para imprimir ingredientes depois
+    for(int i = 0; i < num_ingredientes; i++) {
+        printf("%d. %s\n", i + 1, ingredientes[i]);
+    }
     // Imprimir totais de comparacoes e trocas
-    */
+    printf("\n--------------------------------------------\n");
+    printf("METRICAS DE DESEMPENHO (Bubble Sort):\n");
+    printf("- Total de comparacoes: %d\n", comparacoes);
+    printf("- Total de trocas efetuadas: %d\n", trocas);
+    printf("--------------------------------------------\n");
+    
 
 
     // ---------------------------------------------------------
